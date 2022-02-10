569be5232c25b6634a0c51f2c635fc300e475cfd
maxs5 <93215847+maxs5@users.noreply.github.com>
2022-02-10 20:52:56 +0300

    Initial commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldcalcSum() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+
+        int actual = service.calcSum(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldcalcAvSaleMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = service.calcAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMinSaleMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+
+        long actual = service.minSales(sales);
+
+        assertEquals(expected, actual);
+    }
+}
